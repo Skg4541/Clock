77487a12cef597a14fa5e0862ad119b85dabc0a9
Skg4541 <25678435-Skg4541@users.noreply.replit.com>
2023-09-03 13:53:34 +0000

    Made day function

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
-setInterval(function(){
-let a=new Date();
-let month=a.getMonth();
-let year=a.getFullYear();
-let date=a.getDate();
-let hour=a.getHours();
-let minutes =a.getMinutes();
-let seconds =a.getSeconds()
+setInterval(function() {
+  let a = new Date();
+  let month = a.getMonth();
+  let year = a.getFullYear();
+  let date = a.getDate();
+  let hour = a.getHours();
+  let minutes = a.getMinutes();
+  let seconds = a.getSeconds();
+  let day = a.getDay();
+  let dayofweek = "";
+  switch (day) {
+    case 0: dayofweek = "Sunday";
+      break;
+    case 1: dayofweek = "Monday";
+      break;
+    case 2: dayofweek = "Tuesday";
+      break;
+    case 3: dayofweek = "Wednesday";
+      break;
+    case 4: dayofweek = "Thursday";
+      break;
+    case 5: dayofweek = "Friday";
+      break;
+    case 6: dayofweek = "Saturday";
+      break;
+  }
 
-document.getElementById("Date2").innerHTML = (date.toString() +"/"+ month.toString());
-document.getElementById("Year2").innerHTML = year.toString();
-document.getElementById("Time2").innerHTML = (hour.toString() +":" + minutes.toString() + ":"+ 
- seconds.toString());
-},900)
+  document.getElementById("Date2").innerHTML = (date.toString() + "/" + month.toString());
+  document.getElementById("Day2").innerHTML = dayofweek;
+  document.getElementById("Year2").innerHTML = year.toString();
+  document.getElementById("Time2").innerHTML = (hour.toString() + ":" + minutes.toString() + ":" +
+    seconds.toString());
+}, 900)
 
 //console.log(a.getDate())
 //console.log(a.getMonth())
